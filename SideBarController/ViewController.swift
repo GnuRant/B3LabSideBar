68b93e6f9d1db6756c3cd3dfd2af7f4856b235f6
Luca <luca.dinca@gmail.com>
2014-11-04 16:11:37 +0100

    Initial Commit

---
+++ SideBarController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideBarController
+//
+//  Created by Luca D'incà on 04/11/14.
+//  Copyright (c) 2014 B3LAB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
